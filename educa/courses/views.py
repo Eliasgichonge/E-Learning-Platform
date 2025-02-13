7542a7d3cc1d93ba7f980b12c10a19f8a595fb63
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-13 12:56:41 +0300

    Using mixins for class-based views

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -6,6 +6,18 @@ from django.urls import reverse_lazy
 from .models import Course
 
 
+class OwnerMixin:
+      def get_queryset(self):
+          qs = super().get_queryset()
+          return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+      def form_valid(self, form):
+          form.instance.owner = self.request.user
+          return super().form_valid(form)
+
+
 class ManageCourseListView(ListView):
       model = Course
       template_name = 'courses/manage/course/list.html'
