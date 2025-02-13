27de3cafd8f1ee21d850524aa67b54bfc1aafd23
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-13 09:10:26 +0300

    Creating the Content models

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,3 +1,5 @@
+from symtable import Class
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
@@ -49,3 +51,33 @@ class Content(models.Model):
                                        on_delete=models.CASCADE)
       object_id = models.PositiveIntegerField()
       item = GenericForeignKey('content_type', 'object_id')
+
+class Itembase(models.Model):
+       owner = models.ForeignKey(User,
+                                 related_name='%class)s_realated',
+                                 on_delete=models.CASCADE)
+       title = models.CharField(max_length=250)
+       created = models.DateTimeField(auto_now_add=True)
+       updated = models.DateTimeField(auto_now=True)
+
+       class Meta:
+             abstract = True
+
+       def __str__(self):
+           return self.title
+
+
+class Text(Itembase):
+      content = models.TextField()
+
+
+class File(Itembase):
+      file = models.FileField(upload_to='files')
+
+
+class Image(Itembase):
+      file = models.FileField(upload_to='images')
+
+
+class Video(Itembase):
+      url =models.URLField()
