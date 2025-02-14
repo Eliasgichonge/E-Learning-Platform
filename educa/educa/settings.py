384cccf97171efc2100635caeb58d784c618fcad
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-15 00:32:38 +0300

    Checking cache requests with Django Debug Toolbar

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'courses.apps.CoursesConfig',
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +143,7 @@ CACHES = {
          'LOCATION': '127.0.0.1:11211',
      }
 }
+
+INTERNAL_IPS = [
+       '127.0.0.1',
+]
