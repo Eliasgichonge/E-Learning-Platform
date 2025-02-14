cc62318bad0815630c7ba6ba68236ca3fa0273a1
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-14 21:17:19 +0300

    Registering the models in the administration site

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -12,3 +12,9 @@ class ModuleInline(admin.StackedInline):
 
 
 @admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
