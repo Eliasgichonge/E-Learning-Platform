c7ec51d340d7e91c02785d27cc5ba511e663a9d6
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-14 22:35:38 +0300

    Creating the authentication templates

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for educa project.
+"""educa URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,19 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(),
-         name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'),
-         name='logout'),
+          name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(),
+          name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
 
 if settings.DEBUG:
-   urlpatterns += static(settings.MEDIA_URL,
-                         document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
