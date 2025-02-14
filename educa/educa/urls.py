1d695426411322df9043c98576179deb079f4756
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-14 22:22:48 +0300

    Creating the authentication templates

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -23,7 +23,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(),
          name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='logged_out.html'),
          name='logout'),
     path('admin/', admin.site.urls),
 ]
