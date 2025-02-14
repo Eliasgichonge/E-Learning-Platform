4459b6f597eaa9b5e4a97447be1c7920a45f325f
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-14 21:08:56 +0300

    Building the course models

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -24,4 +24,10 @@ class Course(models.Model):
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
 
