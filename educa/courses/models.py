0f99a0cf9fda24f53b48aa98c13ad654fb9ee117
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-14 21:52:27 +0300

    Adding ordering to module and content objects

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .fields import OrderField
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
