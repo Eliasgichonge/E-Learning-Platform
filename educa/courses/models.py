a67245b1118c6373fe7e78e903f553f8b9674650
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-02-14 21:32:57 +0300

    Creating models for polymorphic content

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Subject(models.Model):
       title = models.CharField(max_length=200)
